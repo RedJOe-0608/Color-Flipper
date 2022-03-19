10c1ded75593b4bcb9e5a35dd1bc8e855c23c373
Jyothi Swaroop Makena <joeswaroopmakena@gmail.com>
2022-03-19 10:09:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const main = document.querySelector(".main");
+const color = document.querySelector(".color");
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+btn.addEventListener("click", () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  main.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
